f64764f4f0cb07324127426213f9ee329b4d13d7
ken88ling <ken88ling@gmail.com>
2021-06-28 17:24:23 +0800

    use hook form

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import React, {
   useRef,
 } from 'react';
 import Outlets from '../components/Outlets';
+import { useForm } from 'react-hook-form';
 
 function index(props) {
   const [outlets, setOutlet] = useState([
@@ -13,62 +14,35 @@ function index(props) {
     { id: '2', name: 'outlet2' },
   ]);
 
-  const [id, setId] = useState();
-  const [name, setName] = useState('');
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    const copy = [...outlets];
-    let newOutlet = {};
-    newOutlet.id = id;
-    newOutlet.name = name;
-    copy.push(newOutlet);
-
-    setOutlet(copy);
-    console.log(`Submitting Name ${id} ${name}`);
+  const { handleSubmit, register, errors } = useForm();
+  const onSubmit = (values) => {
+    setOutlet({ ...outlets, values });
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label>
-          id:
-          <input
-            type='text'
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-          />
-        </label>
-        <label>
-          Outlet Name:
-          <input
-            type='text'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <input type='submit' value='Submit' />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name='outlet'
+          ref={register({
+            required: 'Required',
+          })}
+        />
+        {errors.outlet && errors.outlet.message}
+
+        <input
+          name='username'
+          ref={register({
+            validate: (value) => value !== 'admin' || 'Nice try!',
+          })}
+        />
+        {errors.username && errors.username.message}
+
+        <button type='submit'>Submit</button>
       </form>
-      <Outlets data={outlets} />
+      {/* <Outlets data={outlets} /> */}
     </>
   );
 }
-
-// This function gets called at build time
-// getServerProps vs getStaticProps
-// export async function getStaticProps() {
-//   // Call an external API endpoint to get posts
-
-//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-//   const posts = await res.json();
-
-//   // By returning { props: { posts } }, the Blog component
-//   // will receive `posts` as a prop at build time
-//   return {
-//     props: {
-//       posts,
-//     },
-//   };
-// }
 
 export default index;
